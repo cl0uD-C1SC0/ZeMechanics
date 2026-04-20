dfd5edb54a999a81f0817da68950db76755cf6cc
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-19 22:43:21 -0300

    update: added __init__.py to mark this dir as package

---
+++ app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from app.schemas.ClienteSchema import ClienteSchema
+from app.schemas.VeiculoSchema import VeiculoSchema
+from app.schemas.Peca import Peca
+from app.schemas.Servico import Servico
+from app.schemas.OrdemServico import OrdemServico
