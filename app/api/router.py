20acfbdf12eef7c65044dbfa80f0e13fe218c61a
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-19 22:34:29 -0300

    update: added a router to include the apis

---
+++ app/api/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from app.api.v1 import api_Cliente, api_OrdemServico, api_Peca, api_Servico, api_Veiculo
+
+router = APIRouter()
+
+router.include_router(api_Cliente.router)
+router.include_router(api_Veiculo.router)
