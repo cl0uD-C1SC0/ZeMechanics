8cc924ef4e6b39efd1d425c3d45d72e27aac1f00
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-19 22:39:11 -0300

    update: added a config to auth into SQL Database

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from  pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.getcwd()
+
+dotenv = os.path.join(BASE_DIR, "../../env")
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = str(os.getenv('DATABASE_URL'))
+    SECRET_KEY: str = str(os.getenv('SECRET_KEY'))
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
