c2096bdaf4b05f5359562977a5cc00dbf2f65a33
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-23 20:48:35 -0300

    update: added __init__.py to schemes dir

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,5 +1,5 @@
 from app.schemas.ClienteSchema import ClienteSchema
 from app.schemas.VeiculoSchema import VeiculoSchema
-from app.schemas.Peca import Peca
-from app.schemas.Servico import Servico
-from app.schemas.OrdemServico import OrdemServico
+from app.schemas.Peca import PecaSchema, PecaResponse
+from app.schemas.Servico import ServicoSchema, ServicoResponse
+from app.schemas.OrdemServicoSchema import OrdemDeServicoSchema
