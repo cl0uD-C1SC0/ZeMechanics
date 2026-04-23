9892d18bc8e439493d2937729d62a3234e5a7d0e
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-23 20:43:10 -0300

    update: added Client SQL Model

---
+++ app/domain/models/Cliente_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class Cliente(Base):
+    __tablename__ = "clientes"
+
+    id       = Column(Integer, primary_key=True, index=True)
+    nome     = Column(String(100), nullable=False)
+    cpf      = Column(String(14), unique=True, nullable=False)
+    contato  = Column(String(20), nullable=False)
+    endereco = Column(String(200), nullable=False)
+
+    veiculos = relationship("Veiculo", back_populates="cliente")
+    ordens   = relationship("OrdemDeServico", back_populates="cliente")
+    
