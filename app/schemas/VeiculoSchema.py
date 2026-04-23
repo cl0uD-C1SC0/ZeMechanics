dad7e3284229baad6c3c9dae375267739143830d
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-23 20:49:12 -0300

    update: added VeiculoSchema to use on apis

--- app/schemas/VeiculoSchema.py
+++ app/schemas/VeiculoSchema.py
@@ -1,15 +1,34 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
+from typing import Optional
 class VeiculoSchema(BaseModel):
     modelo: str
+    marca: str
     placa: str
     ano: str
     cliente_id: int
 
-    # def __init__(self, modelo, placa, ano):
-    #     self._modelo = modelo
-    #     self._placa  = placa
-    #     self._ano    = ano
+class AddVehicleReponse(BaseModel):
+    id: int
+    cliente_id: int
+    marca: str
+    modelo: str
+    placa: str
+    ano: str
+    class Config:
+        from_attributes = True
 
-    # def __str__(self):
-    #     return f"Veiculo: {self._modelo}, {self._placa}, {self._ano}"
+class UpdateVehicleSchema(BaseModel):
+    modelo: Optional[str] = None
+    marca: Optional[str] = None
+    ano: Optional[str] = None
+
+class ConsultClientVeiculoResponse(BaseModel):
+    veiculo_id: Optional[int] = Field(default=None, alias="id")
+    marca: Optional[str] = None
+    modelo: Optional[str] = None
+    placa: Optional[str] = None
+    ano: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
