e47b45fe455d7a974250369d1c8d6539409c330d
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-23 20:44:26 -0300

    update: added status os as enums

--- app/domain/enums/status_os.py
+++ app/domain/enums/status_os.py
@@ -1,1 +1,9 @@
-...
+from enum import Enum
+
+class StatusOS(str, Enum):
+    RECEBIDA                = "Recebida"
+    EM_DIAGNOSTICO          = "Em Diagnóstico"
+    AGUARDANDO_APROVACAO    = "Aguardando Aprovação"
+    EM_EXECUCAO             = "Em Execução"
+    FINALIZADA              = "Finalizada"
+    ENTREGUE                = "Entregue"
