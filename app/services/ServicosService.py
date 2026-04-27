fc5d7859b00427bd5f1167fe133316e4684e1d3b
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-27 11:33:12 -0300

    update: added all servicos Service configurations

---
+++ app/services/ServicosService.py
@@ -0,0 +1,29 @@
+from fastapi import HTTPException
+from app.repositories import ServicosRepository
+
+def adicionar_servico(servico, db):
+    return ServicosRepository.add_mechanic_service(servico, db)
+
+def listar_todos_servicos(db):
+    return ServicosRepository.get_all_services(db)
+
+def consultar_servico_especifico(servico_id, db):
+    servico = ServicosRepository.describe_service(servico_id, db)
+    if not servico:
+        raise HTTPException(status_code=404, detail="Serviço não encontrado")
+    return servico 
+
+def atualizar_servico_especifico(servico_id, dados, db):
+    servico = consultar_servico_especifico(servico_id, db)
+
+    if not servico:
+        raise HTTPException(status_code=404, detail="Serviço com esse ID não encontrado")
+    
+    servico_atualizado = ServicosRepository.update_service(servico, dados, db)
+    return servico_atualizado
+
+def remover_servico(servico_id, db):
+    if not consultar_servico_especifico(servico_id, db):
+        raise HTTPException(status_code=404, detail="Serviço com esse ID não encontrado")
+    
+    return ServicosRepository.delete_service(servico_id, db)
