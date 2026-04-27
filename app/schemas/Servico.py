55e423ad7a88fe4f5dc6f7e25119f5e2921b59e3
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-27 11:32:39 -0300

    update: add Servico Schema

--- app/schemas/Servico.py
+++ app/schemas/Servico.py
@@ -1,15 +1,20 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class ServicoSchema(BaseModel):
     nome: str
     preco: float
     descricao: str
+class UpdateServiceSchema(BaseModel):
+    nome: Optional[str] = None
+    preco: Optional[str] = None
+    descricao: Optional[str] = None
+    quantidade: Optional[str] = None
 
 class ServicoResponse(BaseModel):
     id   : int
     nome : str
     preco: float
-
     class Config:
         from_attributes = True
 
