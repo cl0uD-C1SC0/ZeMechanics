36bb1788634d336117ecd62a01be3f755f505954
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-27 11:32:30 -0300

    update: add Peca Schema

--- app/schemas/Peca.py
+++ app/schemas/Peca.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class PecaSchema(BaseModel):
     nome: str
     preco: float
     quantidade: int
+
+class UpdatePecaSchema(BaseModel):
+    nome: Optional[str] = None
+    preco: Optional[str] = None
+    
 
 class PecaResponse(BaseModel):
     id   : int
