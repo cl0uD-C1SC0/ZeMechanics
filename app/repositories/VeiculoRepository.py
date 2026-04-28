b9ac5ed825da3cf1fd3187768d406927a02cb13b
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-28 12:21:56 -0300

    update: added VeiculoRepository functions

---
+++ app/repositories/VeiculoRepository.py
@@ -0,0 +1,46 @@
+from app.domain.models.Veiculo_model import Veiculo as VeiculoModel
+from fastapi import HTTPException
+
+def get_vehicle(placa, db):
+    placa_consultada = db.query(VeiculoModel).filter(VeiculoModel.placa == placa).first()
+    return placa_consultada
+
+def add_veiculo(veiculo, db):
+    novo_veiculo = VeiculoModel(
+        modelo      = veiculo.modelo,
+        marca       = veiculo.marca,
+        placa       = veiculo.placa,
+        ano         = veiculo.ano,
+        cliente_id  = veiculo.cliente_id 
+    )
+
+    db.add(novo_veiculo)
+    db.commit()
+    db.refresh(novo_veiculo)
+    return novo_veiculo
+
+def delete_vehicle(veiculo_placa, db):
+    veiculo = db.query(VeiculoModel).filter(VeiculoModel.placa == veiculo_placa).first()
+
+    if not veiculo:
+        raise HTTPException(status_code=404, detail="Veículo com essa placa não encontrado")
+
+    db.delete(veiculo)
+    db.commit()
+
+    return {"message": "Veículo removido com sucesso"}
+
+def update_vehicle_info(veiculo, dados, db):
+    for campo, valor in dados.model_dump(exclude_none=True).items():
+        setattr(veiculo, campo, valor)
+
+    db.commit()
+    db.refresh(veiculo)
+    return veiculo
+
+def get_all_vehicles(db):
+    veiculos = db.query(VeiculoModel).all()
+
+    if veiculos:
+        return veiculos
+    raise HTTPException(status_code=404, detail="Nenhum veículo foi cadastrado.")
