677885aa4e8cae53b18ecf9361d6a6dcb8b7cf4d
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-28 12:19:04 -0300

    update: adjust on client router config and imports

--- app/api/router.py
+++ app/api/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1 import api_Cliente, api_OrdemServico, api_Peca, api_Servico, api_Veiculo
+from app.api.v1 import api_OrdemServico, api_Peca, api_Servico, api_Veiculo, cliente_api
 
 router = APIRouter()
 
-router.include_router(api_Cliente.router)
+router.include_router(cliente_api.router)
 router.include_router(api_Veiculo.router)
 router.include_router(api_OrdemServico.router)
 router.include_router(api_Peca.router)
