6fbd8c80b36553a448ab854bdb00b1b2bd01cdbd
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-28 09:34:42 -0300

    update: adjust on all api code and improvements to follow best practices

--- app/api/v1/api_Cliente.py
+++ app/api/v1/api_Cliente.py
@@ -1,46 +1,46 @@
 
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
+from typing import Annotated
 
-from app.services import svc_Cliente as cliente_service
 from app.database import get_db
-from app.schemas.ClienteSchema import ClienteSchema, AddClientResponse, ConsultClientResponse, ClienteUpdateSchema
+from app.services import ClienteService as cliente_service
+from app.schemas.ClienteSchema import ClienteSchema, ConsultAllClientsResponse, ConsultClientResponse, ClienteUpdateSchema
 
 router = APIRouter(prefix="/cliente", tags=["Clientes v1"])
+DB = Annotated[Session, Depends(get_db)]
 
-@router.post("/cadastrar_cliente", status_code=status.HTTP_201_CREATED, response_model=AddClientResponse,summary="Cadastrar novo cliente", description="Cadastra um novo cliente com CPF, Nome, Endereco & Contato")
-def cadastrar_cliente(cliente: ClienteSchema, db: Session = Depends(get_db)):
+@router.post("/novo_cliente", status_code=status.HTTP_201_CREATED, summary="Cadastrar novo cliente", description="Cadastra um novo cliente com CPF, Nome, Endereco & Contato")
+def cadastrar_cliente(cliente: ClienteSchema, db: DB):
     novo_cliente = cliente_service.cadastrar_cliente(cliente, db)
-    return {"id": novo_cliente.id, "nome": novo_cliente.nome, "message": "Cliente adicionado"}
+    return novo_cliente
 
-@router.get("/clientes", summary="Listar todos os clientes cadastrados", description="Lista todas as informações, incluindo o ID dos clientes")
-def listar_clientes(db: Session = Depends(get_db)):
+@router.get("/clientes", response_model=list[ConsultAllClientsResponse], summary="Listar todos os clientes cadastrados", description="Lista todas as informações, incluindo o ID dos clientes")
+def listar_clientes(db: DB):
     clientes = cliente_service.listar_clientes(db)
-    if clientes:
-        return clientes
-    raise HTTPException(status_code=404, detail="> Não foi possível listar os clientes")
-
-@router.get("/consultar_cliente/{cpf}", response_model=ConsultClientResponse, summary="Consultar um cliente específico", description="Consultar todos os dados de um cliente com base em um CPF")
-def consultar_cliente(cpf: int, db: Session = Depends(get_db)):
+    return clientes
+    
+@router.get("/{cpf}", response_model=ConsultClientResponse, summary="Consultar um cliente específico", description="Consultar todos os dados de um cliente com base em um CPF")
+def consultar_cliente(cpf: int, db: DB):
     cliente = cliente_service.consultar_cliente(cpf, db)    
     return cliente
 
 @router.get("/{cpf}/veiculos", summary="Listar somente veículos de um Cliente", description="Consulta todos os veículos do cliente que foram cadastrados")
-def listar_veiculos_do_cliente(cpf: int, db: Session = Depends(get_db)):
+def listar_veiculos_do_cliente(cpf: int, db: DB):
     cliente_veiculos = cliente_service.listar_veiculos_cliente(cpf, db)
     return cliente_veiculos.veiculos 
 
-@router.patch("/atualizar_informacoes/{cpf}", summary="Atualizar dados cadastrais", description="Atualizar dados como NOME, CONTATO, ENDERECO")
-def atualizar_cliente(cpf: int, dados: ClienteUpdateSchema, db: Session = Depends(get_db)):
+@router.patch("/{cpf}/atualizar_informacoes", summary="Atualizar dados cadastrais", description="Atualizar dados como NOME, CONTATO, ENDERECO")
+def atualizar_cliente(cpf: int, dados: ClienteUpdateSchema, db: DB):
     dados_atualizados = cliente_service.atualizar_informacao_cliente(cpf, dados, db)
     return dados_atualizados
 
-@router.delete("/remover_cliente/{cpf}", summary="Remover um cliente cadastrado", description="Remover um cliente que foi cadastrado")
-def remover_cliente(cpf: int, db: Session = Depends(get_db)):
+@router.delete("/{cpf}/remover_cliente", summary="Remover um cliente cadastrado", description="Remover um cliente que foi cadastrado")
+def remover_cliente(cpf: int, db: DB):
     cliente_removido = cliente_service.remover_cliente(cpf, db)
     return cliente_removido
 
-@router.patch("/transferir_veiculo/{placa}/{novo_cpf}")
-def transferir_veiculo(novo_cpf: str, placa: str, db: Session = Depends(get_db)):
+@router.patch("/{novo_cpf}/transferir_veiculo/{placa}")
+def transferir_veiculo(novo_cpf: int, placa: str, db: DB):
     veiculo_transferido = cliente_service.transferir_veiculo_cliente(placa, novo_cpf, db)
     return veiculo_transferido
