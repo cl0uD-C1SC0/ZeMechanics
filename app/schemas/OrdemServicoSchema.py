257733f8267afcde146a860b9e0d8037af7cbd69
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-04-29 17:50:08 -0300

    update: removed pecas, services from main schema and removed desnecessary comments

--- app/schemas/OrdemServicoSchema.py
+++ app/schemas/OrdemServicoSchema.py
@@ -1,13 +1,10 @@
 from pydantic import BaseModel
-from app.domain.enums.StatusOS import StatusOS
 from typing import Optional
 from datetime import datetime
 
 class OrdemDeServicoSchema(BaseModel):
     cliente_cpf: int
     veiculo_placa: str
-    peca_ids     : list[int]
-    servico_ids  : list[int]
 
 class OSUpdateSchema(BaseModel):
     cliente_cpf: Optional[int] = None
@@ -36,24 +33,3 @@ class OSResponse(BaseModel):
 
     class Config:
         from_attributes = True
-
-#     def calcular_total(self):
-#         total_servicos = sum(s._preco for s in self._servicos)
-#         total_pecas = sum(p._preco for p in self._pecas)
-
-#         valor_total = float(total_servicos + total_pecas)
-
-#         return f"{valor_total:.2f}"
-
-#     def __str__(self):
-#         servicos = [s._nome for s in self._servicos]
-#         pecas = [p._nome for p in self._pecas]
-
-#         return (
-#             f"CLIENTE: {self._cliente}\n"
-#             f"VEICULO: {self._veiculo}\n"
-#             f"SERVIÇOS: {servicos}\n"
-#             f"PEÇAS: {pecas}\n"
-#             f"STATUS: {self._status_os}\n"
-#             f"TOTAL: R${self.calcular_total()}"
-#         )
