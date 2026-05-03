bc181d69ddf26d100f6ea1a7a763a284555694fd
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-05-03 11:17:33 -0300

    update: adjust servico schma to include validations on preco and update schma

--- app/schemas/Servico.py
+++ app/schemas/Servico.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class ServicoSchema(BaseModel):
     nome: str
-    preco: float
+    preco: float = Field(gt=0)
     descricao: str
 class UpdateServiceSchema(BaseModel):
     nome: Optional[str] = None
-    preco: Optional[str] = None
+    preco: Optional[float] = None
     descricao: Optional[str] = None
 
 class ServicoResponse(BaseModel):
