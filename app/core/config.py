04575674fef12b8b9c6c63aaa98b2a22bf5c504a
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-05-03 17:47:17 -0300

    update: created dependencies to get the current user because JWT

--- app/core/config.py
+++ app/core/config.py
@@ -11,10 +11,12 @@ load_dotenv()
 class Settings(BaseSettings):
     DATABASE_URL: str = str(os.getenv('DATABASE_URL'))
     SECRET_KEY: str = str(os.getenv('SECRET_KEY'))
+    SECRET_KEY_JWT: str = str(os.getenv('SECRET_KEY_JWT'))
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 settings = Settings()
