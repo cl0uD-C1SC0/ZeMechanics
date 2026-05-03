46e5b5bc7d1f78e65802440dc50f385e51499dda
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-05-03 18:05:48 -0300

    removed: deleted all files that won't be used anymore

--- app/domain/enums/status_os.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class StatusOS(str, Enum):
-    RECEBIDA                = "Recebida"
-    EM_DIAGNOSTICO          = "Em Diagnóstico"
-    AGUARDANDO_APROVACAO    = "Aguardando Aprovação"
-    EM_EXECUCAO             = "Em Execução"
-    FINALIZADA              = "Finalizada"
-    ENTREGUE                = "Entregue"
