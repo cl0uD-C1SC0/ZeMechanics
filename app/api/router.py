e90277f9e3e67282c59f426af19e5c077b36d4f8
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-05-03 17:46:09 -0300

    updat: added auth_router to mapped routes

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1 import cliente_api, ordemservico_api, peca_api, servico_api, veiculo_api
+from app.api.v1 import cliente_api, ordemservico_api, peca_api, servico_api, veiculo_api, auth_router
+
 
 router = APIRouter()
 
@@ -8,3 +9,4 @@ router.include_router(veiculo_api.router)
 router.include_router(ordemservico_api.router)
 router.include_router(peca_api.router)
 router.include_router(servico_api.router)
+router.include_router(auth_router.router)
