1d285a8861be76b047662484bb47c90a90291770
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-05-08 01:06:19 -0300

    fix: Fixing email service when running via docker-compose after tests fail

--- app/services/EmailService.py
+++ app/services/EmailService.py
@@ -2,24 +2,29 @@ import smtplib
 from email.mime.text import MIMEText
 import os 
 
-SMTP_SERVER = str(os.getenv('MAIL_SERVER_ADDRESS'))
 
-def enviar_email_aprovacao(os):
 
-    
+def enviar_email_aprovacao(os_obj):
+
+    SMTP_SERVER = str(os.getenv('MAIL_SERVER_ADDRESS'))
+
     msg = MIMEText(f"""
-        Olá {os.cliente.nome}!
+        Olá {os_obj.cliente.nome}!
         
-        Sua OS #{os.id} está aguardando sua aprovação.
+        Sua os_obj #{os_obj.id} está aguardando sua aprovação.
         
         Acesse o link para aprovar:
-        http://localhost:8000/api/v1/ordem_servico/aprovar/{os.id}?cliente_cpf={os.cliente.cpf}
+        http://localhost:8000/api/v1/ordem_servico/aprovar/{os_obj.id}?cliente_cpf={os_obj.cliente.cpf}
         
     """)
 
-    msg["Subject"] = f"OS #{os.id} — Aguardando sua aprovação"
+    msg["Subject"] = f"os_obj #{os_obj.id} — Aguardando sua aprovação"
     msg["From"] = "oficina@zemechanics.com"
-    msg["To"] = f"{os.cliente.email}"
+    msg["To"] = f"{os_obj.cliente.email}"
 
-    with smtplib.SMTP(SMTP_SERVER, 1025) as server:
-        server.sendmail(msg["From"], msg["To"], msg.as_string())
+    try:
+        with smtplib.SMTP(SMTP_SERVER, 1025) as server:
+            server.sendmail(msg["From"], msg["To"], msg.as_string())
+
+    except Exception as e:
+        print(f"Erro ao enviar email: {e}")
